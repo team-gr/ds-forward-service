49a97e33fd9fce9d7cb33b77f926d1e03de6df16
tuananh <tuananh2431998@gmail.com>
2021-03-27 23:29:42 +0700

    update

--- src/rate-limit/rate_limit.go
+++ src/rate-limit/rate_limit.go
@@ -11,33 +11,39 @@ import (
 
 var limiter = rate.NewLimiter(0.1, 1)
 
-func limit(next http.Handler, pq domain.PriorityQueue) http.Handler {
+func limit(next http.Handler, pq *domain.PriorityQueue) http.Handler {
+	go func() {
+		for {
+			if pq.Len() > 0 {
+				h := heap.Pop(pq).(*domain.RequestHandler)
+				next.ServeHTTP(h.Writer, h.Request)
+			}
+		}
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("X-Direct") == "true" {
-			log.Print("direct")
-			for limiter.Allow() == false {
-				time.Sleep(10 * time.Millisecond)
-			}
 			w.Write([]byte("direct"))
-			heap.Push(&pq, &domain.RequestHandler{
+			heap.Push(pq, &domain.RequestHandler{
 				Handler: next,
 				Writer: w,
 				Request: r,
 				Priority:   2,
 			})
 		} else {
-			log.Print("crawl")
-			for limiter.Allow() == false {
-				time.Sleep(10 * time.Millisecond)
-			}
 			w.Write([]byte("crawl"))
-			heap.Push(&pq, &domain.RequestHandler{
+			heap.Push(pq, &domain.RequestHandler{
 				Handler: next,
 				Writer: w,
 				Request: r,
 				Priority:   1,
 			})
 		}
+
+		for limiter.Allow() == false {
+			time.Sleep(10 * time.Millisecond)
+		}
+		next.ServeHTTP(w, r)
 	})
 }
 
@@ -45,23 +51,13 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Print("exec")
+		log.Printf("%v", r)
 		w.Write([]byte("hello"))
-		w.Write([]byte(time.Now().String()))
 	})
 
 	pq := make(domain.PriorityQueue, 0)
 	heap.Init(&pq)
 
-	go func() {
-		for {
-			if pq.Len() > 0 {
-				h := heap.Pop(&pq).(*domain.RequestHandler)
-				h.Handler.ServeHTTP(h.Writer, h.Request)
-			}
-		}
-	}()
-
-
 	log.Println("Listening on :4000...")
-	http.ListenAndServe(":4000", limit(mux, pq))
+	http.ListenAndServe(":4000", limit(mux, &pq))
 }
